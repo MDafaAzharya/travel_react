daf45d2d56d425eed482078ce0b3f44d99128822
Agan Sunarya <agansaja22@gmail.com>
2023-12-01 18:34:23 +0700

    menambhakan file RegisterHoster.jsx dan DetailDestinasi.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,45 @@ import { useState } from 'react'
 import Navbar from './app/component/Navbar'
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Register from './app/component/register';
+import RegisterHoster from './app/component/RegisterHoster';
 import Footer from './app/component/Footer';
 import Choose from './app/component/Choose';
 import Login from './app/component/Login';
+import DetailDestinasi from './app/component/DetailDestinasi';
 function App() {
 
   return (
     <>
       <Router>
-        <Navbar/>
-          <Routes>
-              <Route
-                  exact
-                  path="/"
-                  element={<Register />}
-              />
-              <Route
-                  exact
-                  path="/choose"
-                  element={<Choose />}
-              />
-               <Route
-                  exact
-                  path="/login"
-                  element={<Login />}
-              />
-          </Routes>
-          <Footer/>
+        <Navbar />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={<Register />}
+          />
+          <Route
+            exact
+            path="/choose"
+            element={<Choose />}
+          />
+          <Route
+            exact
+            path="/login"
+            element={<Login />}
+          />
+          <Route
+            exact
+            path="/register-hoster"
+            element={<RegisterHoster />}
+          />
+          <Route
+            exact
+            path="/detail-destinasi"
+            element={<DetailDestinasi />}
+          />
+        </Routes>
+        <Footer />
       </Router>
     </>
   )
